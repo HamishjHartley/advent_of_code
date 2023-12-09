c51625e91aabd2d591a5beb95ecd49ced3c92a2e
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2023-12-09 23:21:04 +0000

    Got part 2 working
    two stars :)

--- day_1/main.py
+++ day_1/main.py
@@ -1,16 +1,32 @@
+import re
+
 #inputting in text file of strings
 with open('C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_1/input.txt') as f:
     lines = f.readlines()
     
 num_list = []
 
-for string in lines:
-    line_value = []
+num_dict = {"one":1,
+            "two" :2,
+            "three":3,
+            "four":4,
+            "five":5,
+            "six":6,
+            "seven":7,
+            "eight":8,
+            "nine":9,
+}    
 
-    for i in range(len(string)):
-        if string[i].isnumeric() == True:
-            line_value.append(string[i])
-    
-    num_list.append(int(line_value[0]+line_value[-1]))
+for line in lines:
+    match = re.findall(r"(?=(one|two|three|four|five|six|seven|eight|nine|\d))", line)
+
+    print(match)
+    #check each line if it contains any of num dict entries
+    for i,element in enumerate(match):
+        if element.isnumeric() == False:
+            match[i] = str(num_dict[element]) 
+
+    print(match)
+    num_list.append(int(match[0]+match[-1]))
 
 print(sum(num_list))
