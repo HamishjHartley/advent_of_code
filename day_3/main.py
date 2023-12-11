03a7d12ee2cdb6134a83eee90779ea0a21ca7b19
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2023-12-11 15:29:40 +0000

    started search_adjecent() function

--- day_3/main.py
+++ day_3/main.py
@@ -21,8 +21,7 @@ input2= [
 ]
 
 #iterate through input list
-#return index of each symbol per line
-
+#return index of each symbol per line as a tuple (line number, line index)
 symbols, numbers = [],[]
 for i,line in enumerate(input1):
     #print(line)
@@ -36,10 +35,32 @@ for i,line in enumerate(input1):
         if num_match != None:
             numbers.append((i,j))
 
+#search all adjecent positions, N, NE, NW, E, S, SE, SW, W for each symbol
+def search_adjecent(symbol:tuple):
+    line_num = symbol[0]
+    index = symbol[1]
 
-#print(symbols)
+    #Dicitonary to store directions
+    direction = {
+        "N": (line_num-1,index),
+        "NE":(line_num-1,index+1),
+        "NW":(line_num-1,index-1),
+        "W":(line_num,index-1),
+        "S":(line_num+1,index),
+        "SE":(line_num+1,index+1),
+        "SW":(line_num+1,index-1),
+        "E":(line_num,index+1)
+
+    }
+    for directions in direction:
+        print(direction[directions])
+#if adjecent position contains number, regex that index on that line and capture full number
+#Save each of these numbers in a list 
+#Sum these numbers 
+
+search_adjecent(symbols[0])
+#print(symbols[0][1])
 #print(numbers)
 
-for i in symbols:
-    print(input1[i])
+
 
