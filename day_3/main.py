5cc9d0edf98bac3efd0fb9c4ceb969c997b21be4
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2023-12-14 10:53:23 +0000

    Overhaul of logic
    Searching adjecent squares per number rather than per symbol

--- day_3/main.py
+++ day_3/main.py
@@ -13,75 +13,65 @@ input1 =["467..114..",
         "664.598..."
 ]
 
-input2= [
-    "..*,.&....",
-    ".*........",
-    "..........",
-    "...*......"
-]
-
-#Returns all adjecent positions, N, NE, NW, E, S, SE, SW, W for a symbol
-def search_adjecent(symbol:tuple, input_list:list):
-    search_pos = []
-    line_num = symbol[0]
-    index = symbol[1]
+#Searches all adjecent positions, N, NE, NW, E, S, SE, SW, W for a given index and line in input_list
+#Returns is_valid boolean
+def search_adjecent(index:int, line_num:int, input_list:list):
+    is_valid = False #Boolean flag 
     line_length = len(input_list[0])
 
     #Dicitonary to store directions
-    direction = {
-        "N": (line_num-1,index),
-        "NE":(line_num-1,index+1),
-        "NW":(line_num-1,index-1),
-        "W":(line_num,index-1),
-        "S":(line_num+1,index),
-        "SE":(line_num+1,index+1),
-        "SW":(line_num+1,index-1),
-        "E":(line_num,index+1)
-    }
+    direction = [
+        (line_num-1,index), #North
+        (line_num-1,index+1), #North East
+        (line_num-1,index-1), #North West
+        (line_num,index+1), #East
+        (line_num,index-1), #West
+        (line_num+1,index), #South
+        (line_num+1,index+1), #South East
+        (line_num+1,index-1) #South West
+    ]
 
     for directions in direction:
-        search_pos.append(direction[directions])
-        x =[direction[directions]][0][1]
-        y =[direction[directions]][0][0]
+        print("Directions",directions[0],directions[1])
+        x = directions[0]
+        y = directions[1]
         #bounds of input list
-        if x and y >= 0 and x and y <=line_length:
-            print(input_list[x][y])
-    return search_pos
-
-#iterate through input list
-#return index of each symbol per line as a tuple (line number, line index)
-def get_symbols(input_list:list):  
-    symbols = []
-    for i,line in enumerate(input_list):
-        for j,char in enumerate(line):
-            #checks if current character in line matches given set
-            char_match = re.search(r"[*,$#&%+]+", char)
-
-            #if character natches set record line index and number in symbols list
+        if x and y >= 0 and x <line_length and y <line_length:
+            print(input_list[y][x], "at", x,"," ,y)
+            char_match = re.search(r"[*,$#&%+]+", input_list[y][x])
+            #if adjecent character is a symbol set is_valid flag to True and break
             if char_match != None:
-                symbols.append((i,j))
-    return symbols
+                is_valid = True
+                break
+    return is_valid
 
+#1.Search input string for numbers, save match object for each in number_list[]
 def get_numbers(input_list:list):
-    number_pos = {} #Dictionary to store each number and ther position 
+    number_list = []
+    valid_numbers = []
 
     for i,line in enumerate(input_list):
         number = re.findall(r"\d+", line)
+
         for numbers in number: 
-            #each number and their index in input list
-            number_index =re.search(numbers, line)
-            number_pos[(number_index.span()[0],number_index.span()[1],i)] = numbers  #Linking the numbers with their positions, and line number
-    return number_pos
+            print("Number: ", numbers)
+            match =re.search(numbers, line)
+            start = match.span()[0]
+            end = match.span()[1]
+            #2. For each number in number_list[]: for each character in number search adjecent, 
+            # if search adjecent == Symbol, set number_valid = True break, then append to valid_numbers[]; 
+            for j in range(end - start):
+                #print("searching adjecent to:", match.group()[i], ", position:", start, i) 
+                #print(line[start+j])
+                is_valid = search_adjecent((start+j), i, input_list) 
+                if is_valid == True:
+                    valid_numbers.append(numbers)
+                    print(numbers, "Is valid")
+                    break
 
-symbols = get_symbols(input1)
-numbers = get_numbers(input1)
-#search_adjecent(symbols[7],input1)
+    return valid_numbers
 
-print(numbers.get((0,3,0)))
+valid_numbers = get_numbers(input1)
+print(valid_numbers)
 
-#if adjecent position contains number, regex that index on that line and capture full number
-#Save each of these numbers in a list 
-#Sum these numbers 
-
-#print(symbols[0][1])
-#print(numbers)
+#3. sum(valid_numbers[])
