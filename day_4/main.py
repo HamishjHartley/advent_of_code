cbc2ae272a3fbd3aa0dbbaf4bab00fcf7a8c53e2
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2023-12-16 21:03:38 +0000

    Tidied up code

--- day_4/main.py
+++ day_4/main.py
@@ -1,10 +1,9 @@
 import re
 #inputting in text file of strings
-input1 = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_4/input.txt",'r').read()
-input1 =input1.split('\n')
-
+input1 = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_4/input.txt",'r').read().split('\n')
 counter = []
 
+#Compares winning and actual numbers 
 def compare_numbers(winning,actual):
     total = 0
     for w_number in winning: 
@@ -16,19 +15,17 @@ def compare_numbers(winning,actual):
 #Populates cards dictionary with n_matches per card
 def populate_cards(input_list:list):
     cards = {}
-
-    for i,line in enumerate(input1):
+    for i,line in enumerate(input_list):
         sep = line.split("|")
         actual = re.findall(r"(\d+)",sep[1])
         winning = re.findall(r"(\d+)", sep[0])
         del winning[0] #To delete first entry in list(Not needed)
-
         total = compare_numbers(winning,actual)
         cards[i+1] =total 
     return cards
 
 cards= populate_cards(input1)
-
+#Recursive function to accumulate total cards
 def process_card(card_number:int):
     if card_number == len(cards):
         return
@@ -40,6 +37,4 @@ def process_card(card_number:int):
 
 for i in range(len(cards)):
     process_card(i+1)
-
 print(sum(counter)+207)
-
