0faf15d23e7bc974d32ee041a443c8cd2561b523
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2023-12-19 18:10:11 +0000

    cassify_hand() and rank_hands() implemented

--- day_7/day7.py
+++ day_7/day7.py
@@ -2,65 +2,61 @@ import re
 #inputting in text file of strings
 input1 = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_7/input.txt",'r').read().split('\n')
 
-hand_val = []
-hand = []
-hand_count = []
-hand_types = []
+hands = []
+hand_counts = []
 
-ranks = [] #ranks of type of hand
-
-#Put hands in order of strength
-for i,line in enumerate(input1):
-    temp = []
-    hand_val.append(line.split(" "))
-    hand.append(re.findall("(A|K|Q|J|T|9|8|7|6|5|4|3|2)", hand_val[i][0]))
-    hand_types.append(set(hand[i])) #converting to a set so only unique values
-    for card in hand_types[i]: 
-        temp.append((hand[i].count(card), card))
-    hand_count.append(temp)
-
-def rank_hand(hand_count:list):
-    n = max(hand_count)[0]
-    #5 kind 
-    if n==5:
-        return 6
-        print("5 of a kind") 
-    # #4 kind
-    if n==4:
-        return 5
-        print("4 of a kind") 
-    # #Full house
-    if len(hand_count) ==2 and n ==3:
-        return 4
-        print("Full house")
-    #3 kind
-    if n == 3:
-        return 3
-        print("3 of a kind")
-    #2 pair
-    if len(hand_count) ==3 and n==2:
-        return 2
-        print("2 pair")
-    #1 pair
-    if len(hand_count) ==4:
-        return 1
-        print("1 pair")
-    #High card
-    if len(hand_count) ==5:
-        return 0
-        print("High card")
-
-#print(hand_count)
-
-for hands in hand_count:
-    ranks.append(rank_hand(hands))
-
-print(ranks)
-#4. Order hands in strength
+#Create data structures 
+for line in input1:
+    sep = line.split(" ")
+    count = []
+    match = re.findall("(A|K|Q|J|T|9|8|7|6|5|4|3|2)", sep[0])
+    card_types = set(match)
+    for types in card_types:
+        count.append(match.count(types))
+    hands.append((match, sep[1]))
+    hand_counts.append(count)
 
 
+def classify_hand(hand_count:list):
+    n = max(hand_count)
+    if n ==5:
+        return 6 #Five of a kind
+    if n ==4:
+        return 5 #Four of a kind
+    if n == 3 and len(hand_count) ==2:
+        return 4 #Full house
+    if n ==3 and len(hand_count) ==3:
+        return 3 #Three of a kind
+    if n ==2 and len(hand_count) ==3:
+        return 2 #Two pair
+    if n ==2 and len(hand_count) ==4:
+        return 1 #One pair
+    if len(hand_count) ==5: 
+        return 0 #High card
+    return None
+
+def rank_hands():
+    sum_winnings = 0
+    types = []
+    totals = []
+    for i,hand in enumerate(hand_counts):
+        hand_type = (classify_hand(hand))
+        types.append((hand_type,hands[i]))
+        totals.append(hand_type)
+
+    # #Hands primarily ordered based on type
+    ordered_n = sorted(totals,reverse=True)
+    ordered_types = sorted(types, reverse=True)
+    n_strongest = ordered_n.count(ordered_n[0])
+    if n_strongest ==1:
+        mulitplier = 5
+        for i,hand in enumerate(ordered_types):
+            sum_winnings +=hand[2] *mulitplier
+    return sum_winnings
+
+rank_hands()
 
 
-
-
-#Determine total winnings, sum each hands bid with rank
+    #If two hands have same type: Secondary ordering, 
+    #first card in each hand is compared, stronger card wins
+    
