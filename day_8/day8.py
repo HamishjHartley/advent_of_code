b7258d024d8dbb93728022640df95ba415efd6f6
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2023-12-30 21:27:23 +0000

    started day 9
    Got first part working

--- day_8/day8.py
+++ day_8/day8.py
@@ -5,27 +5,47 @@ from anytree import Node, RenderTree
 input1 = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_8/input.txt",'r').read().split('\n')
 
 directions = input1[0]
-tree= {}
+node_list = []
 dir_map= {"L":0,
           "R":1}
 
+#https://stackoverflow.com/questions/2358045/how-can-i-implement-a-tree-in-python
+class Tree(object):
+    "Generic tree node."
+    def __init__(self, name='root', children=None):
+        self.name = name
+        self.children = []
+        if children is not None:
+            for child in children:
+                self.add_child(child)
+    def __repr__(self):
+        return self.name
+    
+    def add_child(self, node):
+        assert isinstance(node, Tree)
+        self.children.append(node)
+    
 #Populate tree dictionary
 for i,line in enumerate(input1[2:len(input1)]):
+    #Separating Parent node and children nodes from input text
     sep = line.split(sep=" = ")
     parent = sep[0]
-
     children = re.findall("[A-Z]+",sep[1])
+    #1. Link parent node with previous parent node 
+    curr_node = Tree(parent)
+    #2. Add child nodes to parent node 
+    for child in children: 
+        curr_node.add_child(Tree(child))
+    node_list.append((curr_node,curr_node.children))
 
-    tree[parent] = children
+#Traverse tree
+for i,d in enumerate(directions):
+    curr_search = node_list[0][1][dir_map[d]]
+    print(curr_search)
 
-def search_tree(tree:dict):
-    for i,nodes in enumerate(tree):
-        for direction in directions:
-            curr = tree[nodes][dir_map[direction]]
-            print(curr,i)
-            if curr == "ZZZ":
-                print(i, "Steps required")
-                return i
-            
-print(directions)
-n_steps = search_tree(tree)
+    # for nodes in node_list:
+    #     print(nodes)
+
+#parent = nodes[0]
+# L = nodes[1][0]
+# R = nodes[1][1]
---
+++ day_9/day9.py
@@ -0,0 +1,41 @@
+import re
+#inputting in text file of strings
+input1 = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_9/input.txt",'r').read().split('\n')
+
+#Extrapolate the history for each line of data
+
+#Get difference between each number and append this to a new list
+def get_diff(input:list):
+    diff_list = []
+    for i in range(len(input)-1):
+        difference = input[i+1] - input[i] 
+        diff_list.append(difference)
+    return diff_list
+
+#Checks if all elements of list are 0
+def is_zero(input:list):
+    if all(v == 0 for v in input):
+        return True
+    else:
+        return False
+    
+#Recursive function which processes each sequence of a line until is_zero ==True
+def process_seq(input:list):
+    if is_zero(input) ==True:
+        return input
+    else:
+        diff = get_diff(input)
+        process_seq(diff)
+        print(diff)
+
+
+#1. Make new sequence from the difference at each step of history,
+for i,line in enumerate(input1):
+    print("Line:",i)
+    numbers = list(map(int,re.findall("\d+", line))) #Gets list of numbers, casts from str to int
+
+    process_seq(numbers)
+
+
+
+#2. If the sequence is not all zeros, repeat until sequence is all zeros
