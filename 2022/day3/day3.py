f6ddb1d256cf60ae5427d8da3ef1b91276d093b8
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-01-05 07:23:45 +0000

    day3 part 2 2022

--- 2022/day3/day3.py
+++ 2022/day3/day3.py
@@ -1,11 +1,15 @@
 file = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/2022/day3/input.txt",'r').read().split("\n")
 priority_lst = []
 
-for rucksack in file:
-    first,second = rucksack[:len(rucksack)//2],rucksack[len(rucksack)//2:]
-    unique = set(rucksack)
+#Split file line input every 3 lines
+for i in range(0, len(file),3):
+    first = file[i]
+    second= file[i+1]
+    third = file[i+2]
+    group = first + second+third
+    unique = set(group)
     for char in unique:
-        if char in first and char in second:
+        if char in first and char in second and char in third:
             if char.isupper()==True:
                 priority_lst.append(ord(char)-38)
             else:
