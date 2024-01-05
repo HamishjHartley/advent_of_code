72a881039b19b0b7f2b3786c5cf00aeb894218a6
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-01-05 08:14:18 +0000

    day 4 part 2 2022

--- 2022/day4/day4.py
+++ 2022/day4/day4.py
@@ -5,15 +5,7 @@ for pair in file:
     l1, r1 = list(map(int,elves[0].split("-")))
     l2, r2, = list(map(int,elves[1].split("-")))
     #left 1 > left 2 and right1 < right2
-    if l1 >= l2 and r1 <= r2:
+    if (l1 >= l2 and r1 <= r2) or (l2 >= l1 and r2 <= r1 ) or (r1 >= l2 and l1 <=r2):
         total+= 1
-        print(elves,l1 ,">=", l2, "and", r1, "<=", r2)
-        continue
-    #or left2 > left1 and right2 < right1
-    if l2 >= l1 and r2 <= r1:
-        total+=1
-        print(elves,l2 ,">=", l1, "and", r2, "<=", r1)
-        continue
-
-    
+        print(elves,"overlap")
 print(total)
