aa9ba7052335447903db2ea909bbf3cec8413c6c
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-01-06 11:07:11 +0000

    decoded instruction set

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -1,20 +1,26 @@
 import re
-file = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/2022/day5/input.txt",'r').read().split("\n")
+file = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/2022/day5/input.txt",'r').read()
+c = file.split("\n")
+instructions = file.split("\n\n")[1].split("\n")
+print(instructions)
 
 crate_lst = []
 #Create crate lists
-n = re.findall(r"\d+",file[3])
+n = re.findall(r"\d+",c[3])
 for numbers in n:
     crate_lst.append([])
 
 #1. Populate crate lists
 for i in range(3):
-    crates = re.findall(r"[A-Z]|  [\s]",file[i])
-    print(crates)
+    crates = re.findall(r"[A-Z]|  [\s]",c[i])
     for j,crate in enumerate(crates):
         crate_lst[j].append(crate)
 print(crate_lst)
-    #Combine two spaces into one
-#2. Decode instruction set
 
-#3. Apply instructions to crate listsjhj7
+instructions_list = []
+#2. Decode instruction set
+for move in instructions:
+    instructions_list.append(re.findall("\d",move))
+print(instructions_list)
+
+#3. Apply instructions to crate lists
