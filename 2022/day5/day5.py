208e5129bea9cddafe00158e613f70751ecdd82f
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-01-07 17:14:20 +0000

    working for test case p1

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -2,7 +2,6 @@ import re
 file = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/2022/day5/input.txt",'r').read()
 c = file.split("\n")
 instructions = file.split("\n\n")[1].split("\n")
-print(instructions)
 
 crate_lst = []
 #Create crate lists
@@ -10,25 +9,28 @@ n = re.findall(r"\d+",c[3])
 for numbers in n:
     crate_lst.append([])
 
-#1. Populate crate lists
+#1. Populate crate lists,and reverse them
 for i in range(3):
     crates = re.findall(r"[A-Z]|  [\s]",c[i])
     for j,crate in enumerate(crates):
-        crate_lst[j].append(crate)
+        if crate != "   ":
+            crate_lst[j].append(crate)
+    
+for i in range(3):
+    crate_lst[i].reverse()
 print(crate_lst)
-
 
 #2. Decode instruction set
 #3. Apply instructions to crate lists
-for move in instructions:
+for i,move in enumerate(instructions):
+    print(instructions[i])
     m = re.findall("\d",move)
     m = list(map(int,m)) 
-    m = [x-1 for x in m]
-    print(m)
     amount = m[0]
-    origin = m[1]
-    destination = m[2]
+    origin = m[1]-1
+    destination = m[2]-1
     for i in range(amount):
         crate_lst[destination].append(crate_lst[origin].pop())
-
-print(crate_lst)
+        #print(crate_lst[origin],crate_lst[destination])
+    print(crate_lst)
+        #print("Moved",crate_lst[origin],"to",crate_lst[destination])
