1c50b7594751acc46b0ac1962acb5edbbc5f8b9e
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-01-08 09:04:43 +0000

    fixed regex double digit bug
    algorithm worked the whole time

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -5,18 +5,18 @@ instructions = file.split("\n\n")[1].split("\n")
 
 crate_lst = []
 #Create crate lists
-n = re.findall(r"\d+",c[3])
+n = re.findall(r"\d+",c[8])
 for numbers in n:
     crate_lst.append([])
 
 #1. Populate crate lists,and reverse them
-for i in range(3):
-    crates = re.findall(r"[A-Z]|  [\s]",c[i])
+for i in range(len(crate_lst)):
+    crates = re.findall(r"[A-Z]|   [\s]",c[i])
     for j,crate in enumerate(crates):
-        if crate != "   ":
+        if crate != "   " and crate != "    ":
             crate_lst[j].append(crate)
-    
-for i in range(3):
+
+for i in range(len(crate_lst)):
     crate_lst[i].reverse()
 print(crate_lst)
 
@@ -24,13 +24,14 @@ print(crate_lst)
 #3. Apply instructions to crate lists
 for i,move in enumerate(instructions):
     print(instructions[i])
-    m = re.findall("\d",move)
+    m = re.findall("\d+",move)
     m = list(map(int,m)) 
     amount = m[0]
     origin = m[1]-1
     destination = m[2]-1
+    print(amount,origin,destination)
     for i in range(amount):
         crate_lst[destination].append(crate_lst[origin].pop())
-        #print(crate_lst[origin],crate_lst[destination])
+        print(crate_lst[origin],">>",crate_lst[destination])
+#         #print(crate_lst[origin],crate_lst[destination])
     print(crate_lst)
-        #print("Moved",crate_lst[origin],"to",crate_lst[destination])
