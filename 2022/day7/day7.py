9b3d926662b9ec7b787db45638ffe484fd9ca976
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2024-01-31 12:53:20 +0000

    .

---
+++ 2022/day7/day7.py
@@ -0,0 +1,2 @@
+file = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/2022/day5/input.txt",'r').read().split("\n")
+
--- day_10/day10.py
+++ day_10/day10.py
@@ -1,3 +1,17 @@
-import re
-#inputting in text file of strings
-input1 = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_10/input.txt",'r').read().split('\n')
+file = open("C:/Users/theha/OneDrive/Documents/GitHub/advent_of_code/day_10/input.txt",'r').read().split('\n')
+#How many steps along the loop does it take to get from the starting position 
+#to the point farthest from the starting position?
+
+#(N,S,E,W) 
+
+#1. Define pipe types
+pipes = {(1,-1,0,0):"|",(0,0,-1,1):"-",(1,0,1,0):"L"}
+#2. Identify continuous loop using the pipe types, starting with "S"
+for i,line in enumerate(file):
+    for j,char in enumerate(line):
+        if char =="S":
+            print(i,j)
+#3. Count each step taken in pipe-grid
+
+#4. Half this total
+
